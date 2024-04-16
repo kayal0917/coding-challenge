b81180e3bea635e0b7f0b805a6708a4701c04ad8
kayal0917 <165891293+kayal0917@users.noreply.github.com>
2024-04-16 16:20:58 +0530

    Add files via upload

---
+++ Challenge1.java
@@ -0,0 +1,9 @@
+package com.chainsys.coding;
+public class Challenge1 {
+    public static void main(String[] args) {
+    
+	System.out.println("Hello, World.");
+    System.out.println("Hello, Java.");
+
+}
+}
---
+++ Challenge2.java
@@ -0,0 +1,18 @@
+package com.chainsys.coding;
+import java.util.Scanner;
+public class Challenge2 {
+	public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        int a = scan.nextInt();
+	        int b = scan.nextInt();
+	        int c = scan.nextInt();
+
+
+	        System.out.println(a);
+	        System.out.println(b);
+	        System.out.println(c);
+
+	    }
+	}
+
+
